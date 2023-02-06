ddcf4b5ccbd7b2fdecf8651414e223b0918186db
Tima0202 <enjoyhub02@gmail.com>
2023-02-06 13:05:10 +0600

    game123

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 
 import {Routes, Route} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import home1 from '../../image/home1.png'
+import homebg from '../../image/home-bg.png'
 
 const Home = () => {
     return (
         <div id='home'>
             <div className="container">
                 <div className="home">
-
+                    <div className="block">
+                        <img src={home1} alt=""/>
+                        <img src={homebg} alt=""/>
+                    </div>
+                    <div className="block"></div>
+                    <div className="block"></div>
+                    <div className="block"></div>
+                    <div className="block"></div>
                 </div>
             </div>
         </div>
