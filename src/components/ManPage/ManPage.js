16830e6a941bbffc4dd6d7977f62c7a57353744c
NOTNIK_KG <nasirovasylai@gmail.com>
2023-02-06 14:54:43 +0600

    sdf

--- src/components/ManPage/ManPage.js
+++ src/components/ManPage/ManPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import photo from "../../image/man.png"
 import photo2 from "../../image/woman.png"
 import photo3 from "../../image/girl.png"
+import photo4 from "../../image/foot.png"
 
 const ManPage = () => {
     return (
@@ -9,9 +10,10 @@ const ManPage = () => {
             <div className="container">
                 <div className="manpage">
                     <div className="photo">
-                        <img src={photo} alt="img"/>
-                        <img src={photo2} alt="img"/>
-                        <img src={photo3} alt="img"/>
+                        <img className="man" src={photo} alt="img"/>
+                        <img className="woman" src={photo2} alt="img"/>
+                        <img className="girl" src={photo3} alt="img"/>
+                        <img className="foot" src={photo4} alt="img"/>
 
                     </div>
                 </div>
