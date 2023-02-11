44d9ba3d05eaaf70692f190a29667fd385538af6
Tima0202 <enjoyhub02@gmail.com>
2023-02-11 20:31:48 +0600

    dadwawdag

---
+++ src/components/services/data.js
@@ -0,0 +1,249 @@
+import reaomen1 from "../../image/recomen/Rectangle 2268.png";
+import reaomen2 from "../../image/recomen/Rectangle 2269.png"
+import reaomen3 from "../../image/recomen/Rectangle 2270.png"
+import reaomen4 from "../../image/recomen/Rectangle 2271.png"
+import reaomen5 from "../../image/recomen/Rectangle 2272.png"
+import reaomen6 from "../../image/recomen/Rectangle 2268 (1).png"
+import reaomen7 from "../../image/recomen/Rectangle 2269 (1).png"
+import reaomen8 from "../../image/recomen/Rectangle 2270 (1).png"
+import reaomen9 from "../../image/recomen/Rectangle 2271 (1).png"
+import reaomen10 from "../../image/recomen/Rectangle 2272 (1).png"
+import reaomen11 from "../../image/recomen/Rectangle 2268 (2).png"
+import reaomen12 from "../../image/recomen/Rectangle 2269 (2).png"
+import reaomen13 from "../../image/recomen/Rectangle 2270 (2).png"
+import reaomen14 from "../../image/recomen/Rectangle 2271 (2).png"
+import reaomen15 from "../../image/recomen/Rectangle 2272 (2).png"
+import React from "react";
+
+const Data = [
+    {
+        id: 1,
+        img: reaomen1,
+        price: "2100 сом",
+        desc: "Джинсы момсы / банан"
+    },
+    {
+        id: 2,
+        img: reaomen2,
+        price: "670 сом",
+        desc: "Футболка оверсайз / универсал"
+    },
+    {
+        id: 3,
+        img: reaomen3,
+        price: "1350 сом",
+        desc: "Детский свитер / зимний"
+    },
+    {
+        id: 4,
+        img: reaomen4,
+        price: "2100 сом",
+        desc: "Мужской обувь / классика"
+    },
+    {
+        id: 5,
+        img: reaomen5,
+        price: "2100 сом",
+        desc: "Футболка нарядная / летняя"
+    },
+    {
+        id: 6,
+        img: reaomen6,
+        price: "2100 сом",
+        desc: "Футболка нарядная / летняя"
+    },
+    {
+        id: 7,
+        img: reaomen7,
+        price: "670 сом",
+        desc: "Футболка оверсайз / универсал"
+    },
+    {
+        id: 8,
+        img: reaomen8,
+        price: "1350 сом",
+        desc: "Детский свитер / зимний"
+    },
+    {
+        id: 9,
+        img: reaomen9,
+        price: "2100 сом",
+        desc: "Мужской обувь / классика"
+    },
+    {
+        id: 10,
+        img: reaomen10,
+        price: "2100 сом",
+        desc: "Футболка нарядная / летняя"
+    },
+    {
+        id: 11,
+        img: reaomen11,
+        price: "2100 сом",
+        desc: "Джинсы момсы / банан"
+    },
+    {
+        id: 12,
+        img: reaomen12,
+        price: "670 сом",
+        desc: "Футболка оверсайз / универсал"
+    },
+    {
+        id: 13,
+        img: reaomen13,
+        price: "1350 сом",
+        desc: "Детский свитер / зимний"
+    },
+    {
+        id: 14,
+        img: reaomen14,
+        price: "2100 сом",
+        desc: "Мужской обувь / классика"
+    },
+    {
+        id: 15,
+        img: reaomen15,
+        price: "2100 сом",
+        desc: "Футболка нарядная / летняя"
+    },
+    {
+        id: 16,
+        img: reaomen1,
+        price: "2100 сом",
+        desc: "Джинсы момсы / банан"
+    },
+    {
+        id: 17,
+        img: reaomen2,
+        price: "670 сом",
+        desc: "Футболка оверсайз / универсал"
+    },
+    {
+        id: 18,
+        img: reaomen3,
+        price: "1350 сом",
+        desc: "Детский свитер / зимний"
+    },
+    {
+        id: 19,
+        img: reaomen4,
+        price: "2100 сом",
+        desc: "Мужской обувь / классика"
+    },
+    {
+        id: 20,
+        img: reaomen5,
+        price: "2100 сом",
+        desc: "Футболка нарядная / летняя"
+    },
+    {
+        id: 21,
+        img: reaomen6,
+        price: "2100 сом",
+        desc: "Футболка нарядная / летняя"
+    },
+    {
+        id: 22,
+        img: reaomen7,
+        price: "670 сом",
+        desc: "Футболка оверсайз / универсал"
+    },
+    {
+        id: 23,
+        img: reaomen8,
+        price: "1350 сом",
+        desc: "Детский свитер / зимний"
+    },
+    {
+        id: 24,
+        img: reaomen9,
+        price: "2100 сом",
+        desc: "Мужской обувь / классика"
+    },
+    {
+        id: 25,
+        img: reaomen10,
+        price: "2100 сом",
+        desc: "Футболка нарядная / летняя"
+    },
+    {
+        id: 26,
+        img: reaomen11,
+        price: "2100 сом",
+        desc: "Джинсы момсы / банан"
+    },
+    {
+        id: 27,
+        img: reaomen12,
+        price: "670 сом",
+        desc: "Футболка оверсайз / универсал"
+    },
+    {
+        id: 28,
+        img: reaomen13,
+        price: "1350 сом",
+        desc: "Детский свитер / зимний"
+    },
+    {
+        id: 29,
+        img: reaomen14,
+        price: "2100 сом",
+        desc: "Мужской обувь / классика"
+    },
+    {
+        id: 30,
+        img: reaomen15,
+        price: "2100 сом",
+        desc: "Футболка нарядная / летняя"
+    },
+
+]
+
+export default Data
+
+// <div className="recomen-block">
+//     <div className="block-r">
+//     <img className="man" src={reaomen6} alt="image"/>
+//     <div className="block-content">
+//     <h1>2100 сом</h1>
+// <img src={basket} alt=""/>
+// </div>
+// <h5>Джинсы момсы / банан</h5>
+// </div>
+//
+// <div className="block-r">
+//     <img className="man" src={reaomen7} alt="image"/>
+//     <div className="block-content">
+//         <h1>2100 сом</h1>
+//         <img src={basket} alt=""/>
+//     </div>
+//     <h5>Джинсы момсы / банан</h5>
+// </div>
+//
+// <div className="block-r">
+//     <img className="man" src={reaomen8} alt="image"/>
+//     <div className="block-content">
+//         <h1>2100 сом</h1>
+//         <img src={basket} alt=""/>
+//     </div>
+//     <h5>Джинсы момсы / банан</h5>
+// </div>
+//
+// <div className="block-r">
+//     <img className="man" src={reaomen9} alt="image"/>
+//     <div className="block-content">
+//         <h1>2100 сом</h1>
+//         <img src={basket} alt=""/>
+//     </div>
+//     <h5>Джинсы момсы / банан</h5>
+// </div>
+//
+// <div className="block-r">
+//     <img className="man" src={reaomen10} alt="image"/>
+//     <div className="block-content">
+//         <h1>2100 сом</h1>
+//         <img src={basket} alt=""/>
+//     </div>
+//     <h5>Джинсы момсы / банан</h5>
+// </div>
+// </div>
