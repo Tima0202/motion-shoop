92549bdd4264edb5284762822b6b64f4ca83e813
Tima0202 <enjoyhub02@gmail.com>
2023-02-15 21:06:00 +0600

    dawdaw

---
+++ src/components/services/data2.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import womenClothes1 from "../../image/wom-img2.svg"
+import womenClothes2 from "../../image/wom-img3.svg"
+import womenClothes3 from "../../image/wom-img4.svg"
+import womenClothes4 from "../../image/wom-img5.svg"
+import womenClothes5 from "../../image/t-shirts.jpg"
+import womenClothes6 from "../../image/t-sh.jpg"
+import womenClothes7 from "../../image/modnye.jpg"
+import womenClothes8 from "../../image/kurtka.jpg"
+import womenClothes9 from "../../image/kurtki.jpg"
+import womenClothes10 from "../../image/Rectangle 2270.svg"
+import womenClothes11 from "../../image/Rectangle 2271.svg"
+import womenClothes12 from "../../image/aqua.jpg"
+import womenClothes13 from "../../image/dres.webp"
+import womenClothes14 from "../../image/jens.jpg"
+import womenClothes15 from "../../image/топ.webp"
+
+
+const Data2 = [
+    {
+        id: 1,
+        img: womenClothes1,
+        price: "2100 сом",
+        desc: "Джинсы момсы / банан"
+    },
+    {
+        id: 2,
+        img: womenClothes2,
+        price: "670 сом",
+        desc: "Футболка оверсайз / универсал"
+    },
+    {
+        id: 3,
+        img: womenClothes3,
+        price: "1350 сом",
+        desc: "Детский свитер / зимний"
+    },
+    {
+        id: 4,
+        img: womenClothes4,
+        price: "2100 сом",
+        desc: "Мужской обувь / классика"
+    },
+    {
+        id: 5,
+        img: womenClothes5,
+        price: "2100 сом",
+        desc: "Футболка нарядная / летняя"
+    },
+    {
+        id: 6,
+        img: womenClothes6,
+        price: "2100 сом",
+        desc: "Джинсы момсы / банан"
+    },
+    {
+        id: 7,
+        img: womenClothes7,
+        price: "670 сом",
+        desc: "Футболка оверсайз / универсал"
+    },
+    {
+        id: 8,
+        img: womenClothes8,
+        price: "1350 сом",
+        desc: "Детский свитер / зимний"
+    },
+    {
+        id: 9,
+        img: womenClothes9,
+        price: "2100 сом",
+        desc: "Мужской обувь / классика"
+    },
+    {
+        id: 10,
+        img: womenClothes10,
+        price: "2100 сом",
+        desc: "Футболка нарядная / летняя"
+    },
+    {
+        id: 11,
+        img: womenClothes11,
+        price: "2100 сом",
+        desc: "Джинсы момсы / банан"
+    },
+    {
+        id: 12,
+        img: womenClothes12,
+        price: "670 сом",
+        desc: "Футболка оверсайз / универсал"
+    },
+    {
+        id: 13,
+        img: womenClothes13,
+        price: "1350 сом",
+        desc: "Детский свитер / зимний"
+    },
+    {
+        id: 14,
+        img: womenClothes14,
+        price: "2100 сом",
+        desc: "Мужской обувь / классика"
+    },
+    {
+        id: 15,
+        img: womenClothes15,
+        price: "2100 сом",
+        desc: "Футболка нарядная / летняя"
+    },
+]
