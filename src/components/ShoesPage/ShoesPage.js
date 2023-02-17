4b55af87b70154f58d2f1515b3444bbd43cb219d
nasirova003 <119055316+nasirova003@users.noreply.github.com>
2023-02-17 09:55:27 +0600

    dd (#24)

--- src/components/ShoesPage/ShoesPage.js
+++ src/components/ShoesPage/ShoesPage.js
@@ -1,10 +1,26 @@
+
 import React from 'react';
+
 
 const ShoesPage = () => {
 
 
     return (
-        <div>
+
+        <div id="shoesPage">
+            <div className="container">
+                <div className="shoesPage">
+                    <img src={salam1} alt=""/>
+                    <img src={salam2} alt=""/>
+                    <img src={salam3} alt=""/>
+                    <img src={salam4} alt=""/>
+                    <h1>Женская одежда</h1>
+                </div>
+
+
+                <div className="col text-center after-posts">
+                    <button type="button" className="btn btn-secondary">Показать еще 	&darr;</button>                </div>
+            </div>
 
         </div>
     );
